5732027c84698f212e042f725950a6fadee927b2
gatheuws <gatheuws@305386ed-8323-44ba-8e81-b4e0101fbe5d>
2020-05-12 15:28:42 +0000

    Creation des classes filles d'Element et modification du test du Manager

---
+++ trunk/MyLib/Creature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib
+{
+    public class Creature :Element
+    {
+        public Creature(string nom, string description,string dangerosité) 
+            : base(nom,description)
+        {
+            Dangerosité = dangerosité;
+        }
+        public void Modifier(string dangerosité)
+        {
+            Dangerosité = dangerosité;
+        }
+        public string Dangerosité { get; private set; }
+    }
+}
---
+++ trunk/MyLib/Personnage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib
+{
+    public class Personnage : Element
+    {
+        public Personnage(string nom,string prénom,string description)
+            :base(nom,description)
+        {
+            Prénom = prénom;
+        }
+
+        public void ModifierPrénom(string prénom) => Prénom = prénom; 
+        public string Prénom { get; private set; }
+    }
+}

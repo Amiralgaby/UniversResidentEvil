717bb33031b02042ac6af0b65f881574829a6c8c
gatheuws <gatheuws@305386ed-8323-44ba-8e81-b4e0101fbe5d>
2020-05-20 09:18:40 +0000

    Ajout de IEquatable dans Element.cs et changement de la vue, et nouveau tests sur la liste histoire

--- trunk/MyLib/Manager.cs
+++ trunk/MyLib/Manager.cs
@@ -8,17 +8,14 @@ namespace MyLib
 {
     public class Manager
     {
-        public List<Evenement> Histoire = new List<Evenement>();
+        public LinkedList<Evenement> Histoire = new LinkedList<Evenement>();
         private static ObservableCollection<Element> observableCollection = new ObservableCollection<Element>();
         private readonly ObservableCollection<Element> ListElements = observableCollection;
         
         //Fonction pour la liste d'éléments
         public void AjouterElement(Element e) => ListElements.Add(e);
         public void SupprimerElement(Element e) => ListElements.Remove(e);
-        public void ModifierElement(Element e, string nom, string description,bool favoris)
-        {
-            e.ModifierAttribut(nom, description, favoris);
-        }
+        public void ModifierElement(Element e, string nom, string description,bool favoris) => e.ModifierAttribut(nom, description, favoris);
         public void listerLesElement()
         {
             foreach (Element e in ListElements)
@@ -29,5 +26,20 @@ namespace MyLib
         }
 
         //Fonctions pour l'histoire (la liste d'événement)
+        public void AjouterEvenementAHistoire(Evenement ev) => Histoire.AddLast(ev);
+        public void SupprimerEvenementAHistoire(Evenement ev) => Histoire.Remove(ev);
+        public void ModifierEvenementAHistoire(Evenement ev, string lieu, DateTime date)
+        {
+            ev.ModifierLieu(lieu);
+            ev.ModifierDate(date);
+        }
+        public void listerHistoire()
+        {
+            foreach(Evenement ev in Histoire)
+            {
+                Console.WriteLine(ev.ToString());
+                Console.WriteLine("************");
+            }
+        }
     }
 }

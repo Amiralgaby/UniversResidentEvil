9cd9f8b3bd3939419e99a2a8eecf6c0f2ed60f64
gatheuws <gatheuws@305386ed-8323-44ba-8e81-b4e0101fbe5d>
2020-05-26 15:54:42 +0000

    Ajout des nouveaux UserControl car à chaque fois j'oublie que ce n'est pas automatique

---
+++ branches/UniversRE/UCModification.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniversRE
+{
+    /// <summary>
+    /// Logique d'interaction pour UCModification.xaml
+    /// </summary>
+    public partial class UCModification : UserControl
+    {
+        public Manager Man => (Application.Current as App).LeManager;
+        public UCModification()
+        {
+            InitializeComponent();
+            DataContext = Man;
+        }
+    }
+}
